b3c4de77d760144e31ec302d8c8677fde2e587f7
Brian Chenoweth <brian.michael.chenoweth@gmail.com>
2025-08-04 16:53:51 -0700

    Footer fix

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -4,7 +4,6 @@ import Link from 'next/link';
 
 import { GetStarted } from 'components';
 import { NavigationMenu } from '../';
-
 import styles from './Footer.module.scss';
 
 let cx = classNames.bind(styles);
@@ -22,69 +21,64 @@ export default function Footer({
 }) {
   return (
     <>
-    <GetStarted />
-    <footer className={cx('footer')}>
+      <GetStarted />
+      <footer className={cx('footer')}>
+        {/* Footer Navigation */}
+        <div className="container">
+          <div className={cx('footer-nav-contact-info')}>
+            <div className={cx('footer-nav')}>
+              <h3>Quick Links</h3>
+              <NavigationMenu className={cx('quick')} menuItems={menuItems} />
+            </div>
 
-      {/* Footer Navigation */}
-      <div className="container">
-
-        <div className={cx('footer-nav-contact-info')}>
-          <div className={cx('footer-nav')}>
-            <h3>Quick Links</h3>
-            <NavigationMenu className={cx('quick')} menuItems={menuItems} />
+            <div className={cx('contact-info')}>
+              <Link href="/" legacyBehavior>
+                <a className={cx('cppText')}>
+                  {title ?? 'Cal Poly Partners'}
+                </a>
+              </Link>
+              <a href="tel:7600" className={cx('phone')}>
+                (805) 756-7600
+              </a>
+            </div>
           </div>
 
-          <div className={cx('contact-info')}>
-            <Link href="/" legacyBehavior>
-              <a className={cx('cppText')}>
-                {title ?? 'Cal Poly Partners'}
-              </a>
-            </Link>
-            <a href="tel:7600" className={cx('phone')}>
-              (805) 756-7600
+          <div className={cx('logo-address')}>
+            <div className={cx('logo')}>
+              <Link legacyBehavior href="/">
+                <a title="Home">
+                  <Image
+                    src="/logo.png"
+                    width={400}
+                    height={80}
+                    alt="Cal Poly University logo"
+                    layout="responsive"
+                  />
+                </a>
+              </Link>
+            </div>
+
+            <p>1 Grand Avenue, San Luis Obispo, CA 93407</p>
+
+            <a href="tel:8057561111" className={cx('phone')}>
+              (805) 756-1111
             </a>
-
-          </div>
-        </div>
-
-        <div className={cx('logo-address')}>
-          <div className={cx('logo')}>
-            <Link legacyBehavior href="/">
-              <a title="Home">
-                <Image
-                  src="/logo.png"
-                  width={400}
-                  height={80}
-                  alt="Cal Poly University logo"
-                  layout="responsive"
-                />
-              </a>
-            </Link>
           </div>
 
-          <p>1 Grand Avenue, San Luis Obispo, CA 93407</p>
+          <div className={cx('nav-one')}>
+            <NavigationMenu className={cx('nav')} menuItems={navOneMenuItems} />
+          </div>
 
-          <a href="tel:8057561111" className={cx('phone')}>
-            (805) 756-1111
-          </a>
+          <div className={cx('nav-two')}>
+            <NavigationMenu className={cx('nav')} menuItems={navTwoMenuItems} />
+          </div>
 
+          <div className={cx('copyright')}>
+            &copy; {new Date().getFullYear()} {siteTitle ?? 'Cal Poly Partners'}
+          </div>
         </div>
-
-        {/* 🔥 These are your custom menus */}
-        <div className={cx('nav-one')}>
-          <NavigationMenu className={cx('nav')} menuItems={navOneMenuItems} />
-        </div>
-
-        <div className={cx('nav-two')}>
-          <NavigationMenu className={cx('nav')} menuItems={navTwoMenuItems} />
-        </div>
-
-        <div className={cx('copyright')}>
-          &copy; {new Date().getFullYear()} {siteTitle ?? 'Cal Poly Partners'}
-        </div>
-      </div>
-      {console.log(menuItems, navOneMenuItems, navTwoMenuItems)}
-    </footer>
+        {console.log(menuItems, navOneMenuItems, navTwoMenuItems)}
+      </footer>
     </>
   );
 }
