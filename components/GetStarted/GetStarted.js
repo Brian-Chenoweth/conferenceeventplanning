21766866e2a051f579f3bc7eb91408748b9c979b
Brian Chenoweth <brian.michael.chenoweth@gmail.com>
2025-08-06 14:42:38 -0700

    Randomize H2

--- components/GetStarted/GetStarted.js
+++ components/GetStarted/GetStarted.js
@@ -1,20 +1,25 @@
+import { useMemo } from 'react';
 import { Button } from 'components';
-
 import styles from './GetStarted.module.scss';
 
-
 export default function GetStarted() {
+  // useMemo ensures the heading doesn't change on every re-render
+  const heading = useMemo(() => {
+    const options = ["Let's Get it Started", "Ready to Get Started?"];
+    return options[Math.floor(Math.random() * options.length)];
+  }, []);
+
   return (
     <div className={styles.cta}>
       <div className="wp-block-group centered bg-green has-global-padding is-layout-constrained wp-block-group-is-layout-constrained container">
-        
-        <h2 className="wp-block-heading">Let&apos;s Get it Started</h2>
-        <p>Share the details of your event with us, and together, we&apos;ll start creating the memorable experience that you&apos;re looking for.</p>
-          
+        <h2 className="wp-block-heading">{heading}</h2>
+        <p>
+          Share the details of your event with us, and together, we&apos;ll start
+          creating the memorable experience that you&apos;re looking for.
+        </p>
         <Button href="/contact/" styleType="primary">
-            Get Started
+          Get Started
         </Button>
-        
       </div>
     </div>
   );
