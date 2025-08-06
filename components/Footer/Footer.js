22d403899f70adb50b1caf2b32b4604bf854bc6e
Brian Chenoweth <brian.michael.chenoweth@gmail.com>
2025-08-06 09:25:59 -0700

    Order

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,10 +1,10 @@
 import classNames from 'classnames/bind';
 import Image from 'next/image';
 import Link from 'next/link';
+import styles from './Footer.module.scss';
 
 import { GetStarted } from 'components';
 import { NavigationMenu } from '../';
-import styles from './Footer.module.scss';
 
 const cx = classNames.bind(styles);
 
