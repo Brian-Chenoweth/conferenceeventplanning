49392c6cd3d0c1810100aeafb76d82be305ee431
Brian Chenoweth <brian.michael.chenoweth@gmail.com>
2025-08-06 13:56:50 -0700

    Fix order

--- components/GetStarted/GetStarted.js
+++ components/GetStarted/GetStarted.js
@@ -1,6 +1,3 @@
-import className from 'classnames/bind';
-import Link from 'next/link';
-
 import { Button } from 'components';
 
 import styles from './GetStarted.module.scss';
@@ -10,9 +7,9 @@ export default function GetStarted() {
   return (
     <div className={styles.cta}>
       <div className="wp-block-group centered bg-green has-global-padding is-layout-constrained wp-block-group-is-layout-constrained container">
-        <h2 className="wp-block-heading">Let's Get it Started</h2>
-        <p>Share the details of your event with us, and together, we’ll start creating the memorable experience that you’re looking for.</p>
         
+        <h2 className="wp-block-heading">Let's Get it Started</h2>
+        <p>Share the details of your event with us, and together, we&apos;ll start creating the memorable experience that you&apos;re looking for.</p>
           
         <Button href="/contact/" styleType="primary">
             Get Started
