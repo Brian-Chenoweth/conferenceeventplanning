2f194343bc461291cb2aa74aca01685677e16b3a
Brian Chenoweth <brian.michael.chenoweth@gmail.com>
2025-08-06 13:58:16 -0700

    &apos;

--- components/GetStarted/GetStarted.js
+++ components/GetStarted/GetStarted.js
@@ -8,7 +8,7 @@ export default function GetStarted() {
     <div className={styles.cta}>
       <div className="wp-block-group centered bg-green has-global-padding is-layout-constrained wp-block-group-is-layout-constrained container">
         
-        <h2 className="wp-block-heading">Let's Get it Started</h2>
+        <h2 className="wp-block-heading">Let&apos;s Get it Started</h2>
         <p>Share the details of your event with us, and together, we&apos;ll start creating the memorable experience that you&apos;re looking for.</p>
           
         <Button href="/contact/" styleType="primary">
