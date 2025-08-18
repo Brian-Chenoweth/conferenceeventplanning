30b5081805b4410e19b3a7077049c6a94bbeffa1
Brian Chenoweth <brian.michael.chenoweth@gmail.com>
2025-08-17 18:07:35 -0700

    react/no-unescaped-entities

--- components/HomepageIntro/HomepageIntro.js
+++ components/HomepageIntro/HomepageIntro.js
@@ -9,7 +9,7 @@ export default function HomepageIntro() {
     <section className={`bg-white centered ${styles.homeIntro}`}>
 
         <p>
-          Ready to create something extraordinary? At Cal Poly Conference and Event Planning, we're excited to partner with you to bring your vision to life. Whether it's a conference, workshop, or special event, our team is here to ensure every detail is perfect.
+          Ready to create something extraordinary? At Cal Poly Conference and Event Planning, we&apos;re excited to partner with you to bring your vision to life. Whether it&apos;s a conference, workshop, or special event, our team is here to ensure every detail is perfect.
         </p>
 
         <Button href="/contact/" styleType="primary">
