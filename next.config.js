85598abf577a8f1b6b7efc2de97a0a3140e5a721
Brian Chenoweth <brian.michael.chenoweth@gmail.com>
2026-03-15 19:45:21 -0400

    Lighthouse: Use efficient cache lifetimes

--- next.config.js
+++ next.config.js
@@ -31,4 +31,19 @@ module.exports = withFaust({
   async redirects() {
     return [{ source: '/news/:slug', destination: '/:slug', permanent: true }];
   },
+
+  async headers() {
+    return [
+      {
+        source:
+          '/:path*{\\.(?:jpg|jpeg|png|webp|avif|gif|svg|ico|js|css|woff|woff2)$}',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, stale-while-revalidate=86400',
+          },
+        ],
+      },
+    ];
+  },
 });
